860854b6e82ec93dc4ae669c8016c16b72ab5779
NicolasRuizGiolitoUni <nruiz@uni-bremen.de>
2024-08-22 12:20:56 +0200

    Data update
    added the 19 questions

--- src/assets/data.js
+++ src/assets/data.js
@@ -60,4 +60,132 @@ export const data = [
       { text: "Retired" },
     ],
   },
+  {
+    question: "What is your primary profession or field of study?",
+    type: "open",
+    placeholder: "Type here",
+  },
+  {
+    question:
+      "How often do you use social media apps (e.g., Instagram, Facebook, Twitter) on your smartphone?",
+    type: "multiple",
+    answers: [
+      { text: "Never" },
+      { text: "A few times a month" },
+      { text: "A few times a week" },
+      { text: "Daily" },
+      { text: "Several times a day" },
+    ],
+  },
+  {
+    question: "On average, how long do you spend on social media apps per day?",
+    type: "multiple",
+    answers: [
+      { text: "I don’t use social media apps" },
+      { text: "Less than 5 minutes" },
+      { text: "5-30 minutes" },
+      { text: "30 minutes to 1 hour" },
+      { text: "More than 1 hour" },
+    ],
+  },
+  {
+    question:
+      "Overall, which option better describes your social media use habits?",
+    type: "multiple",
+    answers: [
+      { text: "I use social media too little " },
+      { text: "I use social media about right " },
+      { text: "I use social media too much" },
+    ],
+  },
+  {
+    question:
+      "How often do you find yourself using social media apps longer than you intended?",
+    type: "multiple",
+    answers: [
+      { text: "Never" },
+      { text: "Rarely" },
+      { text: "Sometimes" },
+      { text: "Often" },
+      { text: "Always" },
+    ],
+  },
+  {
+    question:
+      "Have you ever thought about reducing the amount of time you spend on social media?",
+    type: "multiple",
+    answers: [{ text: "Yes" }, { text: "No" }],
+  },
+  {
+    question:
+      "If yes, what motivated you to consider reducing your social media use? (Select all that apply)",
+    type: "checkbox",
+    answers: [
+      { text: "To improve productivity" },
+      { text: "To improve mental health" },
+      { text: "To reduce distractions" },
+      { text: "To increase face-to-face interactions" },
+      { text: "Other" },
+    ],
+  },
+  {
+    question:
+      "On average, how many hours a day do you spend using your smartphone?",
+    type: "multiple",
+    answers: [
+      { text: "Less than 1 hour" },
+      { text: "1-3 hours" },
+      { text: "3-5 hours" },
+      { text: "5-7 hours" },
+      { text: "More than 7 hours" },
+    ],
+  },
+  {
+    question:
+      "How dependent are you on your smartphone for daily activities (e.g., communication, work, studies, seek for directions)?",
+    type: "multiple",
+    answers: [
+      { text: "Not dependent at all" },
+      { text: "Somewhat dependent" },
+      { text: "Moderately dependent" },
+      { text: "Very dependent" },
+      { text: "Extremely dependent" },
+    ],
+  },
+  {
+    question:
+      "Have you ever used digital well-being or screen time management features on your smartphone?",
+    type: "multiple",
+    answers: [{ text: "Yes" }, { text: "No" }],
+  },
+  {
+    question:
+      "Imagine you had to live without your smartphone for a week. What aspects of your daily life would be most affected? (e.g., communictating with loved ones, work, etc)",
+    type: "open",
+    placeholder: "Type here",
+  },
+  {
+    question:
+      "Have you ever considered switching to a non-smartphone (e.g., a basic phone or feature phone)?",
+    type: "multiple",
+    answers: [
+      { text: "Yes" },
+      { text: "No" },
+      { text: "I already use a non-smartphone" },
+    ],
+  },
+  {
+    question:
+      "If you have considered switching to a non-smartphone, what stopped you from making the switch? (Select all that apply)",
+    type: "checkbox",
+    answers: [
+      { text: "Need access to work-related apps" },
+      { text: "Dependence on communication apps (e.g., WhatsApp, Messenger)" },
+      { text: "Fear of missing out (FOMO) on social media" },
+      { text: "Convenience of having everything in one device" },
+      { text: "Entertainment purposes (e.g., streaming, gaming)" },
+      { text: "Concern about losing functionality" },
+      { text: "Other" },
+    ],
+  },
 ];
